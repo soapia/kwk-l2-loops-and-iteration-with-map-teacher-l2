8bdeff031718ea15f35c34287e05e514fb22ba74
sofia ongele <sofiaongele@gmail.com>
2018-07-12 16:59:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution in this file.
+
+function lowerCaseStudentNames(array) {
+  return array.map(function (thing) {
+  thing = thing.toLowerCase()
+  return thing
+  })
+}
+
+function nameToAttributes(array) {
+  return array.map(function (thing) { // makes a function
+  thing = thing.split(" ") // split a string based on a space
+  thing = {firstName: thing[0], lastName: thing[1]} // 
+  return thing
+  })
+}
+
+function attributesToPhrase(array) {
+  return array.map(function (thing) {
+     return `${thing.name} is from ${thing.hometown}`
+  })
+}
+
