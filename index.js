0508eb550bb73a1d1887ce62ee8dff0c35d9616e
sofia ongele <sofiaongele@gmail.com>
2018-07-13 22:51:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,13 +6,28 @@ function lowerCaseStudentNames(array) {
   })
 }
 
-function nameToAttributes(array) {
-  return array.map(function (thing) { // makes a function
-  thing = thing.split(" ") // split a string based on a space
-  thing = {firstName: thing[0], lastName: thing[1]} // 
-  return thing
-  })
+// function nameToAttributes(array) {
+//   return array.map(function (thing) { // makes a function
+//   thing = thing.split(" ") // split a string based on a space
+//   thing = {firstName: thing[0], lastName: thing[1]} // 
+//   return thing
+//   })
+// }
+
+function separateName(studentName){
+  var arrOfNames = studentName.split(' ');
+  var attributes = {
+    firstName: arrOfNames[0],
+    lastName: arrOfNames[1]
+  } 
+  return attributes;
 }
+function nameToAttributes(students){ // sofia checked this is right
+  return students.map(separateName);
+  }
+
+
+
 
 function attributesToPhrase(array) {
   return array.map(function (thing) {
@@ -20,3 +35,5 @@ function attributesToPhrase(array) {
   })
 }
 
+
+console.log(nameToAttributes(["Jane, Doe", "Bob Doe"]))
